46126acf15c540b76a96281485f3bc1289c1be5e
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-21 02:01:54 +0100

    install and setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
