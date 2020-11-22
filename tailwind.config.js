a42afac83fcfbf7f3a02beb714776ad3ab0d2a53
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-22 22:33:54 +0100

    normalize css and setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ['Poppins'] },
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        gray: 'hsl(0, 0%, 75%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        darkBlue: 'hsl(255, 11%, 22%)',
+        darkViolet: 'hsl(260, 8%, 14%)',
+      },
+    },
   },
   variants: {
     extend: {},
