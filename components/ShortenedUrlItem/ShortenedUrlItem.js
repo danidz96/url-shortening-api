61ab4f8f352a6a362eaa2952ff384ad80e782f64
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-30 23:14:16 +0100

    update short link

--- components/ShortenedUrlItem/ShortenedUrlItem.js
+++ components/ShortenedUrlItem/ShortenedUrlItem.js
@@ -12,10 +12,14 @@ function ShortenedUrlItem({ url, shortenedUrl }) {
   return (
     <div className="bg-white rounded-md p-4 lg:flex lg:w-full lg:space-x-6 lg:items-center lg:py-3 lg:px-6">
       <p className="text-violet-dark truncate border-b border-gray-light pb-3 lg:border-none lg:py-3 lg:flex-1">
-        <a href={url}>{url}</a>
+        <a target="_blank" href={url}>
+          {url}
+        </a>
       </p>
       <p className="truncate text-cyan py-3">
-        <a href={shortenedUrl}>{shortenedUrl}</a>
+        <a target="_blank" href={shortenedUrl}>
+          {shortenedUrl}
+        </a>
       </p>
       <Button
         className={`rounded-md w-full h-10 ${
