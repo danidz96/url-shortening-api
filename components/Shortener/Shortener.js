3e6cf4d76887a63be098ea51c3fe43e3896c89f8
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-30 22:13:38 +0100

    get shortened url from api

--- components/Shortener/Shortener.js
+++ components/Shortener/Shortener.js
@@ -1,13 +1,29 @@
-import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useForm, reset } from 'react-hook-form';
 import Button from 'components/Button/Button';
-import { shorten } from 'utils/Shorten';
+import { shorten } from 'utils/shorten';
+import ShortenedUrlList from 'components/ShortenedUrlList/ShortenedUrlList';
 
 function Shortener() {
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, watch, errors, setValue } = useForm();
+  const [shortenedUrlList, setShortenedUrlList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async ({ url }) => {
+    setLoading(true);
+    const response = await shorten(url);
+    setLoading(false);
+    const updatedUrlList = [
+      ...shortenedUrlList,
+      { code: response.code, url, shortenedUrl: response.short_link, status: '' },
+    ];
+    setShortenedUrlList(updatedUrlList);
+    setValue('url', '');
+  };
 
   return (
     <section className="max-w-7xl mx-auto my-16 w-4/5">
-      <form onSubmit={handleSubmit(shorten)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div
           className={`bg-violet bg-shorten-mobile bg-no-repeat bg-right-top p-6 rounded-xl flex flex-col space-y-4 lg:flex-row lg:p-12 lg:space-y-0 lg:bg-shorten-desktop lg:flex-wrap ${
             errors.url && 'lg:pb-6'
@@ -18,7 +34,9 @@ function Shortener() {
             name="url"
             aria-label="Link Input"
             placeholder="Shorten a link here..."
-            className={`p-4 rounded-md flex-1 lg:p-2 ${errors.url && 'border-2 border-red'}`}
+            className={`p-4 rounded-md flex-1 lg:py-2 lg:px-6 ${
+              errors.url && 'border-2 border-red'
+            }`}
             ref={register({
               required: { value: true, message: 'Please add link' },
               pattern: {
@@ -32,11 +50,14 @@ function Shortener() {
               {errors.url.message}
             </span>
           )}
-          <Button type="submit" className="rounded-md py-4 px-12 mt-0 lg:ml-5">
-            Shorten It!
+          <Button type="submit" className="rounded-md py-4 px-12 mt-0 lg:ml-5 lg:w-52">
+            {loading ? 'Loading...' : 'Shorten It!'}
           </Button>
         </div>
       </form>
+      <div className="py-6">
+        <ShortenedUrlList shortenedUrlList={shortenedUrlList} />
+      </div>
     </section>
   );
 }
