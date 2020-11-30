26cea95d18704bb8809dec21b064fec740407f1f
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-30 17:01:49 +0100

    update styles

--- components/ShortenedUrlItem/ShortenedUrlItem.js
+++ components/ShortenedUrlItem/ShortenedUrlItem.js
@@ -2,7 +2,7 @@ import Button from 'components/Button/Button';
 
 function ShortenedUrlItem({ url, shortenedUrl, status }) {
   return (
-    <div className="rounded-md p-4 lg:flex lg:w-full lg:space-x-6 lg:items-center">
+    <div className="bg-white rounded-md p-4 lg:flex lg:w-full lg:space-x-6 lg:items-center lg:py-3 lg:px-6">
       <p className="text-violet-dark truncate border-b border-gray pb-3 lg:border-none lg:py-3 lg:flex-1">
         <a href={url}>{url}</a>
       </p>
