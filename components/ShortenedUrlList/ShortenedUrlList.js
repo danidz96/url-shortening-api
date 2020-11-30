fe08d59c357c4d18e1e8e18557ae20c1554fc210
Dani Domínguez <dani.d.zamorano@gmail.com>
2020-11-30 22:13:02 +0100

    add key

--- components/ShortenedUrlList/ShortenedUrlList.js
+++ components/ShortenedUrlList/ShortenedUrlList.js
@@ -5,6 +5,7 @@ function ShortenedUrlList({ shortenedUrlList }) {
     <div className="space-y-3">
       {shortenedUrlList?.map((shortenedUrlItem) => (
         <ShortenedUrlItem
+          key={shortenedUrlItem.code}
           url={shortenedUrlItem.url}
           shortenedUrl={shortenedUrlItem.shortenedUrl}
           status={null}
